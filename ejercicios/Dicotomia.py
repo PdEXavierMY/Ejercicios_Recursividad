803a0b386d895e30fd560b6a00f40c9aeaa3a1d1
javimiguelez <91721699+Xavitheforce@users.noreply.github.com>
2022-02-27 12:37:30 +0000

    main

--- ejercicios/Dicotomia.py
+++ ejercicios/Dicotomia.py
@@ -1,17 +1,20 @@
-tabla = ["cantar", "estupefacto", "incrédulo", "casa", "perro", "dormir", "leer", "planta", "libro", "soñar", "Word", "carrera", "disfrutar", "Qué?", "odio", "vida", "juega", "suspender", "aprobado", "decepción", "descanso", "alegría", "No", "manchar", "actitud", "Pedro", "hoy", "baúl", "bailar", "seis", "más", "palabras", "casi", "llegar", "negro"]
-tabla_ord = sorted(tabla)
-print(tabla_ord)
-palabra = str(input("Introduce la palabra que quieres encontrar: "))
-
-def busquedadicotomia(n):
-  mitad = int(((len(tabla_ord)-1)/2) - n)
-  if palabra == tabla_ord[mitad]:
-    print("La palabra se encuentra en la posición " + str(mitad+1))
-  elif palabra < tabla_ord[mitad] and palabra > tabla_ord[mitad-1]:
-    print("La palabra introducida no se encuentra en la lista de palabras.")
-  else:
-    if palabra < tabla_ord[mitad]:
-      busquedadicotomia(n+1)
+class dicotomia():
+  tabla = ["cantar", "estupefacto", "incrédulo", "casa", "perro", "dormir", "leer", "planta", "libro", "soñar", "Word", "carrera", "disfrutar", "Qué?", "odio", "vida", "juega", "suspender", "aprobado", "decepción", "descanso", "alegría", "No", "manchar", "actitud", "Pedro", "hoy", "baúl", "bailar", "seis", "más", "palabras", "casi", "llegar", "negro"]
+  tabla_ord = sorted(tabla)
+  print(tabla_ord)
+  palabra = str(input("Introduce la palabra que quieres encontrar: "))
+  
+  def busquedadicotomia(self, n):
+    self.mitad = int(((len(dicotomia().tabla_ord)-1)/2) - n)
+    if dicotomia().palabra == dicotomia().tabla_ord[self.mitad]:
+      print("La palabra se encuentra en la posición " + str((self.mitad)+1))
+    elif dicotomia().palabra < dicotomia().tabla_ord[self.mitad] and dicotomia().palabra > dicotomia().tabla_ord[(self.mitad)-1]:
+      print("La palabra introducida no se encuentra en la lista de palabras.")
     else:
-      busquedadicotomia(n-1)
-busquedadicotomia(0)
+      if dicotomia().palabra < dicotomia().tabla_ord[self.mitad]:
+        dicotomia().busquedadicotomia(n+1)
+      else:
+        dicotomia().busquedadicotomia(n-1)
+
+  def __init__(self):
+    dicotomia().busquedadicotomia(0)
--- main.py
+++ main.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
   inicio = int(input("Elige el ejercicio a realizar(1=Búsqueda por dicotomía,2=Palíndromos,3=Ordenar la bandera): "))
   if inicio == 1 or inicio == 2 or inicio == 3:
     if inicio == 1:
+      Dicotomia().dicotomia().__init__()
     elif inicio == 2:
-      Palindromo().__init__()
+      Palindromos().Palindromo().__init__()
     elif inicio == 3:
+      Bandera().bandera().__init__()
   else:
     print("Has elegido salir o no has introducido correctamente el número del ejercicio.")
