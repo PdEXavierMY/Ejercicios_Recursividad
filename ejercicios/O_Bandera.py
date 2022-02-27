bc71aba29fd0fbdbacb31e1f3cbd248c304eb357
javimiguelez <91721699+Xavitheforce@users.noreply.github.com>
2022-02-27 16:56:26 +0000

    bandera

---
+++ ejercicios/O_Bandera.py
@@ -0,0 +1,20 @@
+import itertools
+class bandera():
+  def extraercolores(n, fichas, color1, color2, color3):
+    if n <= (len(fichas)-1):
+      if fichas[n] == "R":
+        color1.append("R")
+      elif fichas[n] == "A":
+        color2.append("A")
+      elif fichas[n] == "V":
+        color3.append("V")
+      bandera().extraercolores(n+1, fichas, color1, color2, color3)
+
+  def iniciar(self):
+    self.bandera_caotica = ["R", "A", "A", "R", "V", "A", "A", "R", "A", "R", "R", "V", "R", "R", "A", "V", "V"] #Cadena ya separada(de ser introducida tipo [RAARVAA...] simplemente se le pasaría un split)
+    self.color1 = []
+    self.color2 = []
+    self.color3 = []#partimos de conocer los n colores, aunque la cantidad de fichas puede variar
+    bandera().extraercolores(0, self.bandera_caotica, self.color1, self.color2, self.color3)
+    bandera_ordenada = list(itertools.chain(self.color1, self.color2, self.color3))
+    print("La bandera con los colores agrupados queda de la siguiente forma: " + str(bandera_ordenada))
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import sys
 sys.path.insert(1, './ejercicios')
-from ejercicios import Bandera, B_Dicotomia, Palindromos
+from ejercicios import O_Bandera, B_Dicotomia, Palindromos
 
-def __init__(self):
+def __init__():
   if __name__ == "__main__":
-    self.inicio = int(input("Elige el ejercicio a realizar(1=Búsqueda por dicotomía,2=Palíndromos,3=Ordenar la bandera): "))
-    if self.inicio == 1 or self.inicio == 2 or self.inicio == 3:
-      if self.inicio == 1:
+    inicio = int(input("Elige el ejercicio a realizar(1=Búsqueda por dicotomía,2=Palíndromos,3=Ordenar la bandera): "))
+    if inicio == 1 or inicio == 2 or inicio == 3:
+      if inicio == 1:
         B_Dicotomia.dicotomia().iniciar()
-      elif self.inicio == 2:
+      elif inicio == 2:
         Palindromos.Palindromo().iniciar()
-      elif self.inicio == 3:
-        Bandera.bandera().iniciar()
+      elif inicio == 3:
+        O_Bandera.bandera().iniciar()
     else:
       print("Has elegido salir o no has introducido correctamente el número del ejercicio.")
 __init__()
