3ecd3e01750e85072e2ae2c9f4dd9625df8fdb9c
javimiguelez <91721699+Xavitheforce@users.noreply.github.com>
2022-02-27 13:09:59 +0000

    empezar bandera

---
+++ ejercicios/Bandera.py
@@ -0,0 +1,4 @@
+bandera_caotica = ["R", "A", "A", "R", "V", "A", "A", "R", "A", "R", "R", "V", "R", "R", "A", "V", "V"] #Cadena ya separada(de ser introducida tipo [RAARVAA...] simplemente se le pasaría un split)
+bandera_ordenada = []
+
+def extraercolores():
--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import sys
 sys.path.insert(1, './ejercicios')
 from ejercicios import Bandera, B_Dicotomia, Palindromos
 
-def __init__():
+def __init__(self):
   if __name__ == "__main__":
-    inicio = int(input("Elige el ejercicio a realizar(1=Búsqueda por dicotomía,2=Palíndromos,3=Ordenar la bandera): "))
-    if inicio == 1 or inicio == 2 or inicio == 3:
-      if inicio == 1:
+    self.inicio = int(input("Elige el ejercicio a realizar(1=Búsqueda por dicotomía,2=Palíndromos,3=Ordenar la bandera): "))
+    if self.inicio == 1 or self.inicio == 2 or self.inicio == 3:
+      if self.inicio == 1:
         B_Dicotomia.dicotomia().iniciar()
-      elif inicio == 2:
+      elif self.inicio == 2:
         Palindromos.Palindromo().iniciar()
-      elif inicio == 3:
+      elif self.inicio == 3:
         Bandera.bandera().iniciar()
     else:
       print("Has elegido salir o no has introducido correctamente el número del ejercicio.")
